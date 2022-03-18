585ddc4df3b625953875fa4d3d2f024f79029dbf
NguyenQuangHuy282002 <huy282002@gmail.com>
2022-03-18 20:10:21 +0700

    ReactJs to do list

--- src/features/Todo/index.js
+++ src/features/Todo/index.js
@@ -2,9 +2,38 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./style.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
 Todo.propTypes = {};
 
 function Todo(props) {
+
+  const [arr, setArr] = useState([]);
+  const [todo, setTodo] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+      item: todo,
+      status: "Not completed",
+    };
+    setArr([...arr].concat(newTodo));
+    setTodo("");
+  }
+
+  function changeStatus(item) {
+    let completeTask = arr.map((task) => {
+      if (task === item) {
+        task.status = "Completed";
+      }
+      return task;
+    });
+    setArr(completeTask);
+
+  }
+
+  function deleteTask(task) {
+    const removedTask = [...arr].filter(t => t !== task);
+    setArr(removedTask);
+  }
   return (
     <div className="Todo">
       <div>Todos</div>
@@ -16,15 +45,16 @@ function Todo(props) {
         <div className="body">
           <h4>Item</h4>
 
-          <form>
-            <input type="search" required class="form-control" placeholder="What do you want to do?">
+          <form onSubmit={handleSubmit} >
+
+            <input type="search" required className="form-control" placeholder="What do you want to do?" value={todo} onChange={(e) => setTodo(e.target.value)}>
             </input>
+
+            <p>Enter what you want</p>
+
+            <button className="btn btn-primary" type="submit"> Submit
+            </button>
           </form>
-
-          <p>Enter what you want</p>
-
-          <a href="" className="btn btn-primary">Submit</a>
-
         </div>
       </div>
       <div className="todo__task">
@@ -35,57 +65,25 @@ function Todo(props) {
         <table>
           <thead>
             <tr>
-              <th>Item</th>
-              <th>Status</th>
-              <th>Action</th>
+              <th width="40%">Item</th>
+              <th width="20%">Status</th>
+              <th width="40%">Action</th>
             </tr>
           </thead>
 
           <tbody>
-            <tr>
-              <td>Buy Benz</td>
-              <td>Not completed</td>
-              <td>
-                <a href="" className="btn btn-primary">Complete</a>
-                <a href="" className="btn btn-danger">Delete</a>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Buy Benz</td>
-              <td>Not completed</td>
-              <td>
-                <a href="" className="btn btn-primary">Complete</a>
-                <a href="" className="btn btn-danger">Delete</a>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Buy Benz</td>
-              <td>Not completed</td>
-              <td>
-                <a href="" className="btn btn-primary">Complete</a>
-                <a href="" className="btn btn-danger">Delete</a>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Buy Benz</td>
-              <td>Not completed</td>
-              <td>
-                <a href="" className="btn btn-primary">Complete</a>
-                <a href="" className="btn btn-danger">Delete</a>
-              </td>
-            </tr>
-
-            <tr>
-              <td>Buy Benz</td>
-              <td>Not completed</td>
-              <td>
-                <a href="" className="btn btn-primary">Complete</a>
-                <a href="" className="btn btn-danger">Delete</a>
-              </td>
-            </tr>
+            {arr.map(item => (
+              <>
+                <tr>
+                  <td>{item.item}</td>
+                  <td>{item.status}</td>
+                  <td>
+                    <a className="btn btn-primary" onClick={() => changeStatus(item)}>Complete</a>
+                    <a className="btn btn-danger" onClick={() => deleteTask(item)}>Delete</a>
+                  </td>
+                </tr>
+              </>
+            ))}
           </tbody>
         </table>
       </div>
