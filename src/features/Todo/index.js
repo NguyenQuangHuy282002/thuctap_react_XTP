860e26cb3f395795fe2c5ce2e959e9174fb1ca08
NguyenQuangHuy282002 <huy282002@gmail.com>
2022-03-18 10:40:16 +0700

    Create view by reactjs

--- src/features/Todo/index.js
+++ src/features/Todo/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./style.scss";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 Todo.propTypes = {};
 
 function Todo(props) {
@@ -9,10 +9,85 @@ function Todo(props) {
     <div className="Todo">
       <div>Todos</div>
       <div className="todo__add">
-        add
+        <div className="header">
+          Add a task
+        </div>
+
+        <div className="body">
+          <h4>Item</h4>
+
+          <form>
+            <input type="search" required class="form-control" placeholder="What do you want to do?">
+            </input>
+          </form>
+
+          <p>Enter what you want</p>
+
+          <a href="" className="btn btn-primary">Submit</a>
+
+        </div>
       </div>
       <div className="todo__task">
-        task
+        <div className="header">
+          Tasks
+        </div>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Buy Benz</td>
+              <td>Not completed</td>
+              <td>
+                <a href="" className="btn btn-primary">Complete</a>
+                <a href="" className="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Buy Benz</td>
+              <td>Not completed</td>
+              <td>
+                <a href="" className="btn btn-primary">Complete</a>
+                <a href="" className="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Buy Benz</td>
+              <td>Not completed</td>
+              <td>
+                <a href="" className="btn btn-primary">Complete</a>
+                <a href="" className="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Buy Benz</td>
+              <td>Not completed</td>
+              <td>
+                <a href="" className="btn btn-primary">Complete</a>
+                <a href="" className="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Buy Benz</td>
+              <td>Not completed</td>
+              <td>
+                <a href="" className="btn btn-primary">Complete</a>
+                <a href="" className="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
